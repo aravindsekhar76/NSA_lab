cce7c0ad12a2b8c9461f44fb016c62df3cc83dc5
Aravind Sekhar <115882981+aravindsekhar76@users.noreply.github.com>
2023-08-09 03:52:17 +0530

    Update largestofthree.sh

--- NSA_series/largestofthree.sh
+++ NSA_series/largestofthree.sh
@@ -5,7 +5,7 @@ read -p "enter numbers : " n1 n2 n3
 if [ $n1 -gt $n2 ] && [ $n1 -gt $n3 ]
 then
     echo "$n1 is largest"
-elif [ $n2 -gt $n1 ] && [ $n2 -gt $n3 
+elif [ $n2 -gt $n1 ] && [ $n2 -gt $n3 ]
 then 
     echo "$n2 is largest"
 else
